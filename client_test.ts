b046443eff34b780f8e017bedc5572a662738075
Drew Brantley <brantley@us.ibm.com>
2018-11-06 15:20:31 -0600

    fix client

--- client_test.ts
+++ client_test.ts
@@ -2,124 +2,133 @@ import {ScriptEventEmitter, uuid_v4} from "./classes/ScriptEventEmitter.class"
 import {WsEventType, AddExpressEndpointContainer} from "./interfaces/script_loader.interface"
 import {ScriptNetClientObj} from "./interfaces/ScriptnetObj.interface"
 
-// import config_dir from "./helpers/config_dir";
+try{
+    start()
+}catch(e){
 
-// const local_config = config_dir("./config/client_test");
+}
 
-const config = require("config");
+function start(){
 
-const local_config = config.util.loadFileConfigs("./config/client_test")
+    // import config_dir from "./helpers/config_dir";
 
-console.log(local_config);
+    // const local_config = config_dir("./config/client_test");
 
-//const scriptnet_server_obj = local_config.remote_scriptnet_server_obj
-const scriptnet_server_obj = local_config.remote_scriptnet_server_obj
+    const config = require("config");
 
-const scriptnet_client_obj:ScriptNetClientObj = local_config.scriptnet_client_obj;
-scriptnet_client_obj.connection_id = uuid_v4();
+    const local_config = config.util.loadFileConfigs("./config/client_test")
 
-new ScriptEventEmitter(scriptnet_server_obj,scriptnet_client_obj, doneCallback);
+    console.log(local_config);
 
-function doneCallback( script_event_emitter:ScriptEventEmitter ){
+    //const scriptnet_server_obj = local_config.remote_scriptnet_server_obj
+    const scriptnet_server_obj = local_config.remote_scriptnet_server_obj
 
-    console.log( "in doneCallback" );
+    const scriptnet_client_obj:ScriptNetClientObj = local_config.scriptnet_client_obj;
+    scriptnet_client_obj.connection_id = uuid_v4();
 
-    script_event_emitter.getWsClient().on("error", ()=>{
-        console.log('test err')
-        process.exit(0);
-    })
+    new ScriptEventEmitter(scriptnet_server_obj,scriptnet_client_obj, doneCallback);
 
-    console.log('script_event_emitter.ws_client.on("open", ()=>{')
+    function doneCallback( script_event_emitter:ScriptEventEmitter ){
 
-    const x:AddExpressEndpointContainer = {
-        event:{
-            event_type:WsEventType.ADD_EXPRESS_ENDPOINT,
-            uuid: uuid_v4(),
-            data:{
-                router_name:"test123",
-                express_string:"/test123",
-                http_method:"ALL",
-                cloud_event_string:"test_http_event_cloud"
-            }
-        },
-        device_meta_data:{},
-        event_name:WsEventType.ADD_EXPRESS_ENDPOINT,
-    };
-    
-    // add express endpoint that emits event
-    script_event_emitter.emitToCloud( x ); 
+        console.log( "in doneCallback" );
 
-    // register for same event you emit from express
-    script_event_emitter.addRegisteredEvent({
-        cloud_event_string:"test_http_event_cloud",
-        required_keys_table:null,
-        script_event_string:"test_http_event",
-    });
+        script_event_emitter.getWsClient().on("error", ()=>{
+            console.log('test err')
+            process.exit(0);
+        })
 
-    // react to express request 
-    script_event_emitter.on_smart_http( "test_http_event" , async( data )=>{
-    
-        console.log();
-        console.log("test_http_event");
-        console.log(data);
-        console.log();
-        const time = (new Date()).getTime();
-        const client = "client";
-        
-        return {
-            status:200,
-            msg:"😉",
-            type:"text/plain",
-            msg_only:true
+        console.log('script_event_emitter.ws_client.on("open", ()=>{')
+
+        const x:AddExpressEndpointContainer = {
+            event:{
+                event_type:WsEventType.ADD_EXPRESS_ENDPOINT,
+                uuid: uuid_v4(),
+                data:{
+                    router_name:"test123",
+                    express_string:"/test123",
+                    http_method:"ALL",
+                    cloud_event_string:"test_http_event_cloud"
+                }
+            },
+            device_meta_data:{},
+            event_name:WsEventType.ADD_EXPRESS_ENDPOINT,
         };
-    });
-
-    // SECOND TEST 
-
-    const x2:AddExpressEndpointContainer = {
-        event:{
-            event_type:WsEventType.ADD_EXPRESS_ENDPOINT,
-            uuid: uuid_v4(),
-            data:{
-                router_name:"test123",
-                express_string:"/req_test",
-                http_method:"ALL",
-                cloud_event_string:"req_test_cloud"
-            }
-        },
-        device_meta_data:{},
-        event_name:WsEventType.ADD_EXPRESS_ENDPOINT,
-    };
-    
-    // add express endpoint that emits event
-    script_event_emitter.emitToCloud( x2 ); 
-
-    // register for same event you emit from express
-    script_event_emitter.addRegisteredEvent({
-        cloud_event_string:"req_test_cloud",
-        required_keys_table:null,
-        script_event_string:"req_test_client",
-    });
-
-    // react to express request 
-    script_event_emitter.on_smart_http( "req_test_client" , async( data )=>{
-    
-        console.log();
-        console.log("req_test_client");
-        console.log(data);
-        console.log();
-        const time = (new Date()).getTime();
-        const client = "client";
         
-        return {
-            status:200,
-            msg:data,
-            type:"application/json",
-            msg_only:false
+        // add express endpoint that emits event
+        script_event_emitter.emitToCloud( x ); 
+
+        // register for same event you emit from express
+        script_event_emitter.addRegisteredEvent({
+            cloud_event_string:"test_http_event_cloud",
+            required_keys_table:null,
+            script_event_string:"test_http_event",
+        });
+
+        // react to express request 
+        script_event_emitter.on_smart_http( "test_http_event" , async( data )=>{
+        
+            console.log();
+            console.log("test_http_event");
+            console.log(data);
+            console.log();
+            const time = (new Date()).getTime();
+            const client = "client";
+            
+            return {
+                status:200,
+                msg:"😉",
+                type:"text/plain",
+                msg_only:true
+            };
+        });
+
+        // SECOND TEST 
+
+        const x2:AddExpressEndpointContainer = {
+            event:{
+                event_type:WsEventType.ADD_EXPRESS_ENDPOINT,
+                uuid: uuid_v4(),
+                data:{
+                    router_name:"test123",
+                    express_string:"/req_test",
+                    http_method:"ALL",
+                    cloud_event_string:"req_test_cloud"
+                }
+            },
+            device_meta_data:{},
+            event_name:WsEventType.ADD_EXPRESS_ENDPOINT,
         };
-    });
+        
+        // add express endpoint that emits event
+        script_event_emitter.emitToCloud( x2 ); 
+
+        // register for same event you emit from express
+        script_event_emitter.addRegisteredEvent({
+            cloud_event_string:"req_test_cloud",
+            required_keys_table:null,
+            script_event_string:"req_test_client",
+        });
+
+        // react to express request 
+        script_event_emitter.on_smart_http( "req_test_client" , async( data )=>{
+        
+            console.log();
+            console.log("req_test_client");
+            console.log(data);
+            console.log();
+            const time = (new Date()).getTime();
+            const client = "client";
+            
+            return {
+                status:200,
+                msg:data,
+                type:"application/json",
+                msg_only:false
+            };
+        });
 
 
-    console.log("doneCallback finished")
+        console.log("doneCallback finished")
 
+    }
 }
