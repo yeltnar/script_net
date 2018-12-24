5e4da890e998c80d8d57e608e3929df790c7a703
Drew Brantley <brantley@us.ibm.com>
2018-12-23 20:52:33 -0600

    add script net events

--- script_nodes/verifier/verifier.ts
+++ script_nodes/verifier/verifier.ts
@@ -2,6 +2,7 @@ import {ScriptEventEmitter, uuid_v4} from "../../classes/ScriptEventEmitter.clas
 import {WsEventType, AddExpressEndpointContainer, CloudEventContainer, checkCloudEventContainer, EventContainer, EventStrings, LocalEventEntry} from "../../interfaces/script_loader.interface"
 import {ScriptNetClientObj} from "../../interfaces/ScriptnetObj.interface"
 import sha512HexHash from "../../helpers/crypto"
+import {bindCloudEventToLocalEvent, addEvent, addHttpEvent} from "../../helpers/script_net_link"
 
 const {exec, execFile} = require("child_process")
 const config = require("config");
@@ -38,43 +39,86 @@ function do_start(){
 
     function doneCallback( script_event_emitter:ScriptEventEmitter ){
 
-        const verifier_added_container:CloudEventContainer  = {
-            device_meta_data:{},
-            event_name:EventStrings.VERIFIER_CONNECTED,
-            event:{
-                event_type:WsEventType.VERIFIER_CONNECTED,
-                uuid:uuid_v4(),
-                data:{}
-            }
-        };
-        script_event_emitter.emitToCloud( verifier_added_container );
+        (function doItTheOldWay(){
 
-        const add_request_verification:LocalEventEntry = {
-            cloud_event_string:EventStrings.REQUEST_VERIFICATION,
-            required_keys_table:undefined,
-            script_event_string:EventStrings.REQUEST_VERIFICATION,
-        };
-        script_event_emitter.addRegisteredEvent( add_request_verification );
+            const verifier_added_container:CloudEventContainer  = {
+                device_meta_data:{},
+                event_name:EventStrings.VERIFIER_CONNECTED,
+                event:{
+                    event_type:WsEventType.VERIFIER_CONNECTED,
+                    uuid:uuid_v4(),
+                    data:{}
+                }
+            };
+            script_event_emitter.emitToCloud( verifier_added_container );
+    
+            const add_request_verification:LocalEventEntry = {
+                cloud_event_string:EventStrings.REQUEST_VERIFICATION,
+                required_keys_table:undefined,
+                script_event_string:EventStrings.REQUEST_VERIFICATION,
+            };
+            script_event_emitter.addRegisteredEvent( add_request_verification );
+    
+            script_event_emitter.on_smart( EventStrings.REQUEST_VERIFICATION, async( data )=>{
+    
+                let result = false;
+    
+                let {script_net_connector_token, ws_request_data} = data.event.data;
+    
+                script_net_connector_token = sha512HexHash(script_net_connector_token);
+    
+                if( script_net_connector_token!==undefined && ws_request_data!==undefined ){
+    
+                    result = checkToken( script_net_connector_token, ws_request_data )
+                    return {result};
+    
+                }else{
+                    result = false;
+                    const err = "need more fields";
+                    return {result, err};
+                }
+            });
 
-        script_event_emitter.on_smart( EventStrings.REQUEST_VERIFICATION, async( data )=>{
+        })();
 
-            let result = false;
+        // verify message (check if the message is allowed through)
+        bindCloudEventToLocalEvent( script_event_emitter, EventStrings.VERIFY_MESSAGE_CLOUD, EventStrings.VERIFY_MESSAGE );
+        addEvent( script_event_emitter, EventStrings.VERIFY_MESSAGE, ( message_obj:CloudEventContainer ):Promise<object>=>{
 
-            let {script_net_connector_token, ws_request_data} = data.event.data;
+            return new Promise((resolve, reject)=>{
 
-            script_net_connector_token = sha512HexHash(script_net_connector_token);
+                const result = checkMessage(message_obj);
 
-            if( script_net_connector_token!==undefined && ws_request_data!==undefined ){
+                resolve({result});
+            })
 
-                result = checkToken( script_net_connector_token, ws_request_data )
-                return {result};
-
-            }else{
-                result = false;
-                const err = "need more fields";
-                return {result, err};
-            }
         });
+
+        setTimeout(()=>{
+
+            const cloud_event_container:CloudEventContainer = {
+                device_meta_data:{}, 
+                event_name:EventStrings.VERIFY_MESSAGE_CLOUD, 
+                event:{
+                    event_type: WsEventType.PLAIN,
+                    uuid: uuid_v4(),
+                    data: "don't _need_ anything"
+                }
+            };
+
+            script_event_emitter.emitToCloudPromise( cloud_event_container ).then(( resp_data )=>{
+
+                console.log()
+                console.log("resp_data");
+                console.log(resp_data);
+
+            }).catch(()=>{
+
+                console.log("didn't");
+
+            });
+
+        }, 2000);
     }
 
 }
@@ -109,6 +153,19 @@ function checkToken( script_net_connector_token:string, ws_request_data:object )
     fs.writeFileSync( file_path, JSON.stringify(master_obj, null, 2) );
 
     return approved;
+}
+
+function checkMessage( message_obj:CloudEventContainer ){
+
+    console.log()
+    console.log("here we go....")
+    console.log()
+    console.log(message_obj)
+    console.log()
+    console.log("...there we went")
+    console.log()
+
+    return true;
 }
 
 export default start;
